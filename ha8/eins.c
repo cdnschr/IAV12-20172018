24fd3f61cc8ac6f253bc86d42a149a2c6fd61c7a
cdnschr <33294485+cdnschr@users.noreply.github.com>
2018-02-28 23:22:39 +0100

    Update eins.c

--- ha8/eins.c
+++ ha8/eins.c
@@ -4,6 +4,17 @@
 #include <math.h>
 
 int main(void){
+double start = 0.0;
+double end = 180.0;
+int part = 5;
+double step;
+
+step = (end - start) / part;
+
+for (int i = 0; i < part; i++){
+  printf("%.2f\n", (start+=step) );
+  }
+  
 
   return 0;
 }

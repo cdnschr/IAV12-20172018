1784d6b6431843b0f282d048d597c13787d086f9
cdnschr <holybunghole7@gmail.com>
2018-07-16 21:41:56 +0200

    geht schon mal fast wie gedacht...

--- ha9/dateizugriff3.c
+++ ha9/dateizugriff3.c
@@ -2,33 +2,56 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define DATEILAENGE 50
+
 int main(void){
 
 FILE *fp;
 double werte[100];
-int i;
+double fliess;
+int i, tmp;
 
 for (i = 0; i < 99; i++){
-  werte[i] = ((double)rand() / RAND_MAX)*(9.99-19.98)+4.99;   // Fließkomma Zahlen von
+  werte[i] = ((double)rand() / RAND_MAX)*(9.99-19.98)+4.99;
   printf("%.2lf\n", werte[i] );
 }
 
 srand(time(NULL));
-fp = fopen("aufgabe3.txt", "w");
+fp = fopen("aufgabe3.txt", "w");  // Datei wird erstellt
 
 if(fp == NULL) {
 	printf("Datei konnte nicht geoeffnet werden.\n");
+  exit(1);
 }
 else{
-
-  for (i = 0; i < 49; i++){
+  tmp = 0;
+  for (i = 0; i < DATEILAENGE; i++){
     fprintf(fp, "%.2lf", werte[i]);
-    if(i%2 == 0){
+    if(tmp == 0){
       fprintf(fp, "\t%.2lf", werte[i]);
+      tmp = 1;
+    }
+    if(tmp == 1){
+      tmp = 0;
     }
     fprintf(fp, "\n");
   }
   printf("Datei wurde erstellt.\n");
+  fclose(fp);
+}
+
+fp = fopen("aufgabe3.txt", "r"); //Datei wird ausgelesen
+
+if(fp == NULL) {
+	printf("Datei konnte nicht geoeffnet werden.\n");
+  exit(2);
+}
+else{
+  for (i = 0; i < DATEILAENGE*2; i++){
+    fscanf(fp, "%lf\n", &fliess);
+    printf("Ausgelesener Wert %d: %.2lf\n", i+1, fliess);
+  }
+  printf("Datei wurde ausgelesen.\n");
   fclose(fp);
 }
   return 0;
